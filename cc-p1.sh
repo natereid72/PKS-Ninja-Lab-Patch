151b6b108aebfebcfbd01c369c704f17ad621825
Nate Reid <45366367+natereid72@users.noreply.github.com>
2018-12-22 14:46:17 -0500

    Update cc-p1.sh

--- cc-p1.sh
+++ cc-p1.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #Define Ops Manager and PKS releases to install
 sed -i.bak '/opsman\_major\_minor\_version: \^2\\.3.*$/c\opsman_major_minor_version: ^2\\.3\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
 sed -i.bak '/pks\_major\_minor\_version: \^1\\.2.*$/c\pks_major_minor_version: ^1\\.2\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
@@ -13,10 +15,11 @@ sed -i.bak '/pks_tile_syslog/s/^/#/' ~/nsx-t-ci-pipeline/pipelines/pks-params.ym
 sed -i.bak  '/mdns4/c\hosts:  files  dns' /etc/nsswitch.conf
 
 #Unused block - Placeholder for anything that may require vSphere config changes
-#curl -k -i -u administrator@vsphere.local:VMware1! -X POST -c cookie-jar.txt https://vcenter/rest/com/vmware/cis/session
-#curl -k -i -b cookie-jar.txt https://vcenter/rest/vcenter/vm 
+#curl -k -i -u administrator@vsphere.local:VMware1! -X POST -c cookie-jar.txt https://vcsa-01a.corp.local/rest/com/vmware/cis/session
+#curl -k -i -b cookie-jar.txt https://vcsa-01a.corp.local/rest/vcenter/... 
 
 #Switch to home directory post-script
 cd ~
 ping vcsa-01a.corp.local -c 1
+echo ""
 echo "Patched: PKS Pipeline, clo-vm DNS client config."
