c45db4ef91d315ae1751adae36adbb145613e8a3
Nate Reid <45366367+natereid72@users.noreply.github.com>
2018-12-21 20:12:40 -0500

    Delete cc-bandaid.sh

--- cc-bandaid.sh
+++
@@ -1,3 +0,0 @@
-sed -i '/pks_syslog_properties/,/Finished configuring syslog properties/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/task.sh
-sed -i '/PKS_SYSLOG/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/task.yml
-sed -i  '/mdns4/c\hots:  files  dns' /etc/nsswitch.conf
