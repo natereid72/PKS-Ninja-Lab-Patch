5dd9a45121e5c7d13d00792fd953b2b052ef9b43
Nate Reid <45366367+natereid72@users.noreply.github.com>
2018-12-22 00:08:06 -0500

    Update cc-p1.sh

--- cc-p1.sh
+++ cc-p1.sh
@@ -1,3 +1,9 @@
+#Define Ops Manager and PKS releases to install
+sed -i.bak '/opsman\_major\_minor\_version: \^2\\.3.*$/c\opsman_major_minor_version: ^2\\.3\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
+sed -i.bak '/pks\_major\_minor\_version: \^1\\.2.*$/c\pks_major_minor_version: ^1\\.2\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
+
+pks\_major\_minor\_version: \^1\\.2.\*\$ 
+
 #Comment out incompatible property references in pks pipeline
 sed -i.bak '/pks_syslog_properties/,/Finished configuring syslog properties/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/task.sh
 sed -i.bak '/PKS_VRLI_ENABLED/,/\<fi\>/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/config-pks-1.1.sh
@@ -6,7 +12,7 @@ sed -i.bak '/PKS_SYSLOG/s/^/#/' ~/nsx-t-ci-pipeline/pipelines/install-pks-pipeli
 sed -i.bak '/pks_tile_syslog/s/^/#/' ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
 
 #Correct name service switch config for cli-vm dns name resolution
-sed -i  '/mdns4/c\hots:  files  dns' /etc/nsswitch.conf
+sed -i  '/mdns4/c\hosts:  files  dns' /etc/nsswitch.conf
 
 #Unused block - Placeholder for anything that may require vSphere config changes
 #curl -k -i -u administrator@vsphere.local:VMware1! -X POST -c cookie-jar.txt https://vcenter/rest/com/vmware/cis/session
