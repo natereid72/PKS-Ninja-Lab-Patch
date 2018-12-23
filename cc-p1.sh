03f382fe4be9a702c9e86153ff22177d4f8eb10f
Nate Reid <45366367+natereid72@users.noreply.github.com>
2018-12-23 01:39:54 -0500

    Update cc-p1.sh

--- cc-p1.sh
+++ cc-p1.sh
@@ -12,11 +12,11 @@ mv ~/nsx-t-ci-pipeline/pipelines/install-pks-pipeline.yml.bak ~/nsx-t-ci-pipelin
 mv ~/etc/nsswitch.conf.bak ~/etc/nsswitch.con
 fi
 
-#Define Ops Manager and PKS releases to install
+#Define Ops Manager and PKS releases to install - OpsMan 2.3.0 / PKS 1.2.0
 sed -i.bak '/opsman\_major\_minor\_version: \^2\\.3.*$/c\opsman_major_minor_version: ^2\\.3\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
 sed -i '/pks\_major\_minor\_version: \^1\\.2.*$/c\pks_major_minor_version: ^1\\.2\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
 
-#Comment out incompatible property references in pks pipeline
+#Comment out property references in pks pipeline
 sed -i.bak '/pks_syslog_properties/,/Finished configuring syslog properties/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/task.sh
 sed -i.bak '/PKS_VRLI_ENABLED/,/\<fi\>/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/config-pks-1.1.sh
 sed -i.bak '/PKS_SYSLOG/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/task.yml
