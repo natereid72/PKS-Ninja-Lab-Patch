d63956bef3a806869d52a0dc350c28e3dda4ef5a
Nate Reid <45366367+natereid72@users.noreply.github.com>
2018-12-23 00:27:43 -0500

    Update cc-p1.sh

--- cc-p1.sh
+++ cc-p1.sh
@@ -14,14 +14,14 @@ fi
 
 #Define Ops Manager and PKS releases to install
 sed -i.bak '/opsman\_major\_minor\_version: \^2\\.3.*$/c\opsman_major_minor_version: ^2\\.3\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
-sed -i.bak '/pks\_major\_minor\_version: \^1\\.2.*$/c\pks_major_minor_version: ^1\\.2\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
+sed -i '/pks\_major\_minor\_version: \^1\\.2.*$/c\pks_major_minor_version: ^1\\.2\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
 
 #Comment out incompatible property references in pks pipeline
 sed -i.bak '/pks_syslog_properties/,/Finished configuring syslog properties/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/task.sh
 sed -i.bak '/PKS_VRLI_ENABLED/,/\<fi\>/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/config-pks-1.1.sh
 sed -i.bak '/PKS_SYSLOG/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/task.yml
 sed -i.bak '/PKS_SYSLOG/s/^/#/' ~/nsx-t-ci-pipeline/pipelines/install-pks-pipeline.yml
-sed -i.bak '/pks_tile_syslog/s/^/#/' ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
+sed -i '/pks_tile_syslog/s/^/#/' ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
 
 #Correct name service switch config for cli-vm dns name resolution
 sed -i.bak  '/mdns4/c\hosts:  files  dns' /etc/nsswitch.conf
@@ -31,7 +31,5 @@ sed -i.bak  '/mdns4/c\hosts:  files  dns' /etc/nsswitch.conf
 #curl -k -i -b cookie-jar.txt https://vcsa-01a.corp.local/rest/vcenter/...
 
 #Switch to home directory post-script
+echo "Pathced PKS pipeline and cli-vm DNS client"
 cd ~
-ping vcsa-01a.corp.local -c 1
-echo ""
-echo "Patched: PKS Pipeline, cli-vm DNS client config."
