3c729755646bed1afbe9abf03ea9047a0368b43a
Nate Reid <45366367+natereid72@users.noreply.github.com>
2018-12-23 00:20:55 -0500

    Update cc-p1.sh

--- cc-p1.sh
+++ cc-p1.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+#Check to see if script has already run and reset to original state before running again
+file="~/nsx-t-ci-pipeline/pipelines/pks-params.yml.bak"
+if [ -f "$file" ]
+then
+mv ~/nsx-t-ci-pipeline/pipelines/pks-params.yml.bak ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
+mv ~/nsx-t-ci-pipeline/tasks/config-pks/task.sh.bak ~/nsx-t-ci-pipeline/tasks/config-pks/task.sh
+mv ~/nsx-t-ci-pipeline/tasks/config-pks/config-pks-1.1.sh.bak ~/nsx-t-ci-pipeline/tasks/config-pks/config-pks-1.1.sh
+mv ~/nsx-t-ci-pipeline/tasks/config-pks/task.yml.bak ~/nsx-t-ci-pipeline/tasks/config-pks/task.yml
+mv ~/nsx-t-ci-pipeline/pipelines/install-pks-pipeline.yml.bak ~/nsx-t-ci-pipeline/pipelines/install-pks-pipeline.yml
+mv ~/etc/nsswitch.conf.bak ~/etc/nsswitch.con
+fi
+
 #Define Ops Manager and PKS releases to install
 sed -i.bak '/opsman\_major\_minor\_version: \^2\\.3.*$/c\opsman_major_minor_version: ^2\\.3\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
 sed -i.bak '/pks\_major\_minor\_version: \^1\\.2.*$/c\pks_major_minor_version: ^1\\.2\\.0$'  ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
@@ -16,7 +28,7 @@ sed -i.bak  '/mdns4/c\hosts:  files  dns' /etc/nsswitch.conf
 
 #Unused block - Placeholder for anything that may require vSphere config changes
 #curl -k -i -u administrator@vsphere.local:VMware1! -X POST -c cookie-jar.txt https://vcsa-01a.corp.local/rest/com/vmware/cis/session
-#curl -k -i -b cookie-jar.txt https://vcsa-01a.corp.local/rest/vcenter/... 
+#curl -k -i -b cookie-jar.txt https://vcsa-01a.corp.local/rest/vcenter/...
 
 #Switch to home directory post-script
 cd ~
