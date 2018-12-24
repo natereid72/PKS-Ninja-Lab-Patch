1245c12a70d9fc4fe9647f5c97691e5e60dd26e7
Nate Reid <45366367+natereid72@users.noreply.github.com>
2018-12-23 23:38:02 -0500

    Update cc-p1.sh

--- cc-p1.sh
+++ cc-p1.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-#Leave marker on first run. If it has run before, reset files to original state before running again
+#Check if script has already ran
 file="~/nsx-t-ci-pipeline/.cc-p1.ran"
 if [ ! -f "$file" ]
 then
+#First run. Leave ran marker
 touch ~/nsx-t-ci-pipeline/.cc-p1.ran
+#Add some aliases to make common tasks easier
+alias pksli='pks login -a pks.corp.local -u pks-admin -p VMware1! --skip-ssl-validation'
+alias pksgc='pks get-credentials'
+echo "alias pksli='pks login -a pks.corp.local -u pks-admin -p VMware1! --skip-ssl-validation'" >> ~/.bashrc
+echo "alias pksgc='pks get-credentials'" >> ~/.bashrc
 else
+#Script has previously ran, reset config file to original state before running again
 mv ~/nsx-t-ci-pipeline/pipelines/pks-params.yml.bak ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
 mv ~/nsx-t-ci-pipeline/tasks/config-pks/task.sh.bak ~/nsx-t-ci-pipeline/tasks/config-pks/task.sh
 mv ~/nsx-t-ci-pipeline/tasks/config-pks/config-pks-1.1.sh.bak ~/nsx-t-ci-pipeline/tasks/config-pks/config-pks-1.1.sh
@@ -25,7 +32,7 @@ sed -i.bak '/PKS_SYSLOG/s/^/#/' ~/nsx-t-ci-pipeline/tasks/config-pks/task.yml
 sed -i.bak '/PKS_SYSLOG/s/^/#/' ~/nsx-t-ci-pipeline/pipelines/install-pks-pipeline.yml
 sed -i '/pks_tile_syslog/s/^/#/' ~/nsx-t-ci-pipeline/pipelines/pks-params.yml
 
-#Fix configure pks pipeline that causes k8s clusters to be created in mgmta01
+#Fix configure-pks pipeline that causes k8s clusters to be created in mgmta01
 sed -i 's/"value": $pks_vcenter_cluster_list/"value": "RegionA01-COMP01"/' ~/nsx-t-ci-pipeline/tasks/config-pks/config-pks-1.1.sh
 
 #Correct name service switch config for cli-vm dns name resolution
@@ -34,15 +41,6 @@ sed -i.bak  '/mdns4/c\hosts:  files  dns' /etc/nsswitch.conf
 #Unused block - Placeholder for anything that may require vSphere config changes
 #curl -k -i -u administrator@vsphere.local:VMware1! -X POST -c cookie-jar.txt https://vcsa-01a.corp.local/rest/com/vmware/cis/session
 #curl -k -i -b cookie-jar.txt https://vcsa-01a.corp.local/rest/vcenter/...
-
-#Add some aliases to make common tasks easier
-if [ ! -f "$file" ]
-then
-alias pksli='pks login -a pks.corp.local -u pks-admin -p VMware1! --skip-ssl-validation'
-alias pksgc='pks get-credentials'
-echo "alias pksli='pks login -a pks.corp.local -u pks-admin -p VMware1! --skip-ssl-validation'" >> ~/.bashrc
-echo "alias pksgc='pks get-credentials'" >> ~/.bashrc
-fi
 
 #Switch to home directory post-script
 echo "=========================================="
